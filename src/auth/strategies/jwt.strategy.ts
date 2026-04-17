79a010ed39ec6a101a880b0bca84fae7fa76acec
Valentino Juan <valentinojuan46@gmail.com>
2026-04-17 20:13:46 +0700

    fix: jwt strategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -7,30 +7,21 @@ import { PrismaService } from '../../prisma/prisma.service';
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private prisma: PrismaService) {
     super({
-      // Ngambil token dari header: "Authorization: Bearer <token>"
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false, // Tolak kalau token udah expired
-      secretOrKey: process.env.JWT_SECRET!, // Rahasia dari .env
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET!,
     });
   }
 
-  // 1. Tambahkan role di deklarasi payload
   async validate(payload: { sub: string; email: string; role: string }) {
-    
-    // 2. Cek database untuk memastikan user masih hidup/belum diblokir
     const user = await this.prisma.user.findUnique({
       where: { id: payload.sub },
     });
-
-    // Kalau user udah dihapus dari DB tapi tokennya masih ada, tolak!
     if (!user) throw new UnauthorizedException('Akses ditolak, user tidak ditemukan');
-
-    // 3. Jangan return `user` mentah-mentah karena ada password-nya.
-    // Return data yang penting aja. Ini akan otomatis nempel di `req.user`
     return { 
       userId: user.id, 
       email: user.email, 
-      role: user.role // <-- Role dibawa biar gampang dicek sama Guard
+      role: user.role
     };
   }
 }
