35991f4429abe0674f0881a72dff0723ac19b574
Valentino Juan <valentinojuan46@gmail.com>
2026-04-17 23:43:29 +0700

    fix: add database url in prisma

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('Revobank API')
-    .setDescription('Dokumentasi API untuk sistem perbankan sederhana')
+    .setDescription('Banking System API Documentation')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
